e23a7e20aa8cfc59bde495d2d7e80431878fff1e
mcolvin <colvin.mike@gmail.com>
2016-11-17 20:25:08 -0600

    presentor mode
    added presentor mode by ioslides

--- Course-Materials/compile.R
+++ Course-Materials/compile.R
@@ -2,6 +2,7 @@
 setwd('C:/Users/mcolvin/Google Drive/WFA8433-Natural-Resource-Decision-Making/Course-Materials')
 # RENDER SITE
 rmarkdown::render_site()# build website
+
 # COPY FILES TO DOCS FOR GITHUB.IO
 system(paste("xcopy", 
     '"C:/Users/mcolvin/Google Drive/WFA8433-Natural-Resource-Decision-Making/Course-Materials/_site"', 
