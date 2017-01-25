73af45d8e9326e5242e36135638ad99c73594541
mcolvin <colvin.mike@gmail.com>
2017-01-25 15:04:48 -0600

    pre class 6 commit
    pre class 6 commit

---
+++ Course-Materials/travel-cpt.R
@@ -0,0 +1,50 @@
+
+
+mns<- c(10.6,11.2,11.3,11.3)
+time_eff<- c(0,
+
+xx<- matrix(c(
+    14.835,	    18.6156,	20.5737,	19.53,	    15.4169,	11.0289,
+    14.9721,	19.4322,	20.721,	    18.9121,	16.255,	    9.70766,
+    14.0849,	18.6246,	20.2557,    19.4998,	15.8417,	11.6933,
+    14.1734,	18.174,	    19.5312,	19.3352,	16.2235,	12.5626),
+    nrow=4,byrow=TRUE)
+
+    barplot(xx[1,],las=1,beside=TRUE,
+        #legend.text=c("a","b","c",'d'),
+        ylab="Probability",ylim=c(0,25),
+        names.arg=c("0-5","5-10","10-15","15-20","20-25","25+"))
+       box()
+       
+    barplot(xx[2,],las=1,beside=TRUE,
+        #legend.text=c("a","b","c",'d'),
+        ylab="Probability",ylim=c(0,25),
+        names.arg=c("0-5","5-10","10-15","15-20","20-25","25+"))
+       box()
+       
+      barplot(xx[3,],las=1,beside=TRUE,
+        #legend.text=c("a","b","c",'d'),
+        ylab="Probability",ylim=c(0,25),
+        names.arg=c("0-5","5-10","10-15","15-20","20-25","25+"))
+       box()     
+       
+      barplot(xx[4,],las=1,beside=TRUE,
+        #legend.text=c("a","b","c",'d'),
+        ylab="Probability",ylim=c(0,25),
+        names.arg=c("0-5","5-10","10-15","15-20","20-25","25+"))
+       box()     
+
+    
+
+## MORE COMPLICATED BIGGER TREE.    
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
